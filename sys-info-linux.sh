4bdc9857ce859958ca758cfb71cc70d77bd866d6
Sergey Baitalokha <62718971+gitDicki@users.noreply.github.com>
2025-11-14 22:00:58 +0500

    Create sys-info-linux.sh

---
+++ sys-info-linux.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+get_os_info() {
+    echo "OS: $(lsb_release -d | cut -f2-)"
+    echo "Kernel: $(uname -r)"
+    echo "Architecture: $(uname -m)"
+}
+
+get_user_info() {
+    echo "Hostname: $(hostname)"
+    echo "User: $(whoami)"
+}
+
+get_memory_info() {
+    free_ram=$(free -m | awk '/Mem:/ {print $4}')
+    total_ram=$(free -m | awk '/Mem:/ {print $2}')
+    total_swap=$(free -m | awk '/Swap:/ {print $2}')
+    free_swap=$(free -m | awk '/Swap:/ {print $4}')
+
+    echo "RAM: ${free_ram}MB free / ${total_ram}MB total"
+    echo "Swap: ${total_swap}MB total / ${free_swap}MB free"
+}
+
+get_cpu_info() {
+    cpu_count=$(nproc)
+    load_avg=$(cat /proc/loadavg | awk '{print $1, $2, $3}')
+
+    echo "Processors: ${cpu_count}"
+    echo "Load average: ${load_avg}"
+}
+
+get_disk_info() {
+    echo "Drives:"
+    df -h
+}
+
+get_virtual_memory() {
+    vmalloc_total=$(grep -i 'vmalloctotal' /proc/meminfo | awk '{print $2}')
+
+    echo "Virtual memory: ${vmalloc_total} kB"
+}
+
+get_os_info
+get_user_info
+get_memory_info
+get_virtual_memory
+get_cpu_info
+get_disk_info
